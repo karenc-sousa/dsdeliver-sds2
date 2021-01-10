eedc75bd7f1baf49d012d94acd2e144bfa82f917
karenc-sousa <karenc.fsousa@gmail.com>
2021-01-10 13:54:39 -0300

    Search Order Location

--- front-web/src/Orders/OrderLocation.tsx
+++ front-web/src/Orders/OrderLocation.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import AsyncSelect from 'react-select/async';
 import { fetchLocalMapBox } from '../api';
+import { OrderLocationdata } from './types';
 
 
 const initialPosition = {
@@ -18,7 +19,11 @@ type Place = {
     };
 }
 
-function OrderLocation() {
+type Props = {
+  onChangeLocation: (location: OrderLocationdata) => void;
+}
+
+function OrderLocation( {onChangeLocation}: Props) {
     const [address, setAddress] = useState<Place>({
         position: initialPosition
     });
@@ -42,11 +47,11 @@ function OrderLocation() {
       
       const handleChangeSelect = (place: Place) => {
         setAddress(place);
-       //onChangeLocation({
-       //  latitude: place.position.lat,
-       //  longitude: place.position.lng,
-       //  address: place.label!
-       //});
+        onChangeLocation({
+          latitude: place.position.lat,
+          longitude: place.position.lng,
+          address: place.label!
+        });
       };
 
     return (
@@ -66,7 +71,8 @@ function OrderLocation() {
                 </div>
                 <MapContainer 
                     center={address.position} 
-                    zoom={13} 
+                    zoom={15}
+                    key={address.position.lat} 
                     scrollWheelZoom
                 >
                   <TileLayer
@@ -75,7 +81,7 @@ function OrderLocation() {
                   />
                   <Marker position={address.position}>
                     <Popup>
-                      Meu marcardor
+                      {address.label}
                     </Popup>
                   </Marker>
                 </MapContainer>
